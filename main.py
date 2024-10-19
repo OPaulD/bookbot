7720234daeb4e8060af7b937fa3c75fb962cb3c5
OPaulD <omota.paul@gmail.com>
2024-10-20 00:53:17 +0100

    add output

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     number_of_individual_characters = count_characters(text)
-    print(f"\n{word_count} words found in the document.")
-    print(number_of_individual_characters)
+    sorted_characters = sort_characters(number_of_individual_characters)
 
+    print(f"\n--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+
+    for i in sorted_characters:
+        if not i["char"].isalpha():
+            continue
+        print(f"The '{i['char']}' character was found {i['num']} times")
+
+    print("\n--- End report ---")
 
 
 def get_book_text(path):
@@ -16,6 +24,16 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def sort_characters(character_dict):
+    unsorted_characters = []
+    sorted_characters = []
+    for i in character_dict:
+        unsorted_characters.append({"char":i, "num":character_dict[i]})
+    
+    sorted_characters = sorted(unsorted_characters, key= lambda d: d["num"], reverse=True)
+    return sorted_characters
 
 
 def count_characters(text):
