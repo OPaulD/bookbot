66bb5838a5ac78c5647ae4e616b7451df275b424
OPaulD <omota.paul@gmail.com>
2024-10-20 00:07:42 +0100

    add character dictionary

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     book_path = 'books/frankenstein.txt'
     text = get_book_text(book_path)
     word_count = count_words(text)
+    number_of_individual_characters = count_characters(text)
     print(f"\n{word_count} words found in the document.")
+    print(number_of_individual_characters)
+
 
 
 def get_book_text(path):
@@ -13,6 +16,18 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def count_characters(text):
+    characters_count = {}
+    lower_text = text.lower()
+
+    for i in lower_text:
+        if i in characters_count:
+            characters_count[i] += 1
+        else:
+            characters_count[i] = 1
+    return characters_count
 
 
 main()
